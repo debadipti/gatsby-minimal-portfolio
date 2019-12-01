a8b3feeff7c6bf3b80e9760e7e2e3ff8637e4718
Debadipti Patra <pdebadipti95@gmail.com>
2019-12-01 11:41:30 +0530

    added works and contacts pages

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const ContactPage = () => (
+  <Layout>
+    <h1>Contact me</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsum
+      pariatur fugit magnam illum, facilis consectetur mollitia maiores nihil
+      laborum?
+    </p>
+  </Layout>
+);
+
+export default ContactPage;
---
+++ src/pages/works.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const WorksPage = () => (
+  <Layout>
+    <h1>My Works</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsum
+      pariatur fugit magnam illum, facilis consectetur mollitia maiores nihil
+      laborum?
+    </p>
+  </Layout>
+);
+
+export default WorksPage;
